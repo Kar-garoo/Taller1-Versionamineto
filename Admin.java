23598fb460909938af87b7ec050b18bf4e2314f0
Sergio Alejandro Diaz Pinilla <seradiazpin@unal.edu.co>
2015-03-11 20:49:37 -0500

    Constructor, geters y seters de las clases User y Adim

--- Admin.java
+++ Admin.java
@@ -1,3 +1,28 @@
 public class Admin extends User{
 
+	private int level;
+	private double rating;
+
+ 	public Admin(String name, String lastname, int age, String username, String password, int level, double rating) {
+        super(name, lastname, age, username, password);
+        this.level = level;
+        this.rating = rating;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
 }
