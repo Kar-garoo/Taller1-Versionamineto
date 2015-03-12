ba1d7714b8d3eadca3b0075c40b59a07dd6cce45
Sergio Alejandro Diaz Pinilla <seradiazpin@unal.edu.co>
2015-03-11 21:40:21 -0500

    La relacion entre Admin y Forum agregada como un arreglo de Forum los cuales son moderados.

--- Admin.java
+++ Admin.java
@@ -2,12 +2,14 @@ public class Admin extends User{
 
 	private int level;
 	private double rating;
+	private Forum [] moderate;
 
- 	public Admin(String name, String lastname, int age, String username, String password, int level, double rating) {
+	public Admin(int level, double rating, Forum[] moderate, String name, String lastname, int age, String username, String password) {
         super(name, lastname, age, username, password);
         this.level = level;
         this.rating = rating;
-    }
+        this.moderate = moderate;
+    } 
 
     public int getLevel() {
         return level;
@@ -23,6 +25,14 @@ public class Admin extends User{
 
     public void setRating(double rating) {
         this.rating = rating;
+    }
+
+    public Forum[] getModerate() {
+        return moderate;
+    }
+
+    public void setModerate(Forum[] moderate) {
+        this.moderate = moderate;
     }
 
 }
