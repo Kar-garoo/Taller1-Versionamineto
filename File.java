407b989c1317b74c6084502ddd1024b8a88d394c
Sergio Alejandro Diaz Pinilla <seradiazpin@unal.edu.co>
2015-03-11 21:06:25 -0500

    File con atributos, geters , seters y constructor

--- File.java
+++ File.java
@@ -1,3 +1,45 @@
 public class File{
 	
+	private String fileType;
+    private Byte[] content;
+    private double size;
+
+    public File(String fileType, Byte[] content, double size) {
+        this.fileType = fileType;
+        this.content = content;
+        this.size = size;
+    }
+    
+    public File download (){
+        return null;
+        
+    }
+    
+    public void share (){
+        
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public Byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(Byte[] content) {
+        this.content = content;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
 }
