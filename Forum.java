a337bf0eaea4a8cc1c98d13d418e1e4cf8b491de
Miguel <miatorresch@unal.edu.co>
2015-03-12 12:07:09 -0500

    - Clase Forum Completa

--- Forum.java
+++ Forum.java
@@ -1,3 +1,44 @@
-public class Forum{
+import java.util.Date;
+
+public class Forum {
+	private String name;
+	private Date datecreated;
+	private String category;
+	private Post[] posts;
+	
+	
+	
+	
+	public Forum(String name, Date datecreated, String category, Post[] posts) {
+		this.name = name;
+		this.datecreated = datecreated;
+		this.category = category;
+		this.posts = posts;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getDatecreated() {
+		return this.datecreated;
+	}
+	public void setDatecreated(Date datecreated) {
+		this.datecreated = datecreated;
+	}
+	public String getCategory() {
+		return this.category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public Post[] getPosts() {
+		return this.posts;
+	}
+	public void setPosts(Post[] posts) {
+		this.posts = posts;
+	}
+	
 	
 }
